e1a37816e9974a8905ca64333e192c6b0494303d
renzo <renzobenitez137@gmail.com>
2026-07-08 16:38:40 -0300

    Agrego menus de navegacion (principal, solicitudes, datos)

---
+++ complement/menu.py
@@ -0,0 +1,25 @@
+import os
+
+def menu_principal():
+    print("1. SOLICITUDES")
+    print("2. DATOS")
+    print("3. SALIR")
+
+
+def menu_solicitudes():
+    print("1. AGREGAR USUARIO")
+    print("2. BORRAR USUARIO")
+    print("3. RESERVAR LIBRO")
+    print("4. INGRESAR LIBRO")
+    print("5. VOLVER")
+
+
+def menu_datos():
+    print("1. USUARIOS REGISTRADOS")
+    print("2. LIBROS")
+    print("3. PRESTAMOS")
+    print("4. VOLVER")
+
+
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
